9637b58b80374ecd28de6cf6a0f41f68bb444e24
Ana Bittencourt <anajuliabit@gmail.com>
2025-04-28 16:49:03 -0300

    fmt

--- src/token/WormholeTrustedSender.sol
+++ src/token/WormholeTrustedSender.sol
@@ -34,15 +34,10 @@ contract WormholeTrustedSender is IWormholeTrustedSender {
     /// @dev Updates the list of trusted senders
     /// @param _trustedSenders The list of trusted senders, allowing one
     /// trusted sender per chain id
-    function _addTrustedSenders(
-        TrustedSender[] memory _trustedSenders
-    ) internal {
+    function _addTrustedSenders(TrustedSender[] memory _trustedSenders) internal {
         unchecked {
             for (uint256 i = 0; i < _trustedSenders.length; i++) {
-                _addTrustedSender(
-                    _trustedSenders[i].addr,
-                    _trustedSenders[i].chainId
-                );
+                _addTrustedSender(_trustedSenders[i].addr, _trustedSenders[i].chainId);
             }
         }
     }
@@ -51,48 +46,28 @@ contract WormholeTrustedSender is IWormholeTrustedSender {
     /// @param trustedSender The trusted sender to add
     /// @param chainId The chain id of the trusted sender to add
     function _addTrustedSender(address trustedSender, uint16 chainId) internal {
-        require(
-            trustedSenders[chainId].add(addressToBytes(trustedSender)),
-            "WormholeTrustedSender: already in list"
-        );
+        require(trustedSenders[chainId].add(addressToBytes(trustedSender)), "WormholeTrustedSender: already in list");
 
-        emit TrustedSenderUpdated(
-            chainId,
-            trustedSender,
-            true /// added to list
-        );
+        emit TrustedSenderUpdated(chainId, trustedSender, true);
+        /// added to list
     }
 
     /// @notice remove a trusted sender
     /// @param trustedSender The trusted sender to remove
     /// @param chainId The chain id of the trusted sender to remove
-    function _removeTrustedSender(
-        address trustedSender,
-        uint16 chainId
-    ) internal {
-        require(
-            trustedSenders[chainId].remove(addressToBytes(trustedSender)),
-            "WormholeTrustedSender: not in list"
-        );
+    function _removeTrustedSender(address trustedSender, uint16 chainId) internal {
+        require(trustedSenders[chainId].remove(addressToBytes(trustedSender)), "WormholeTrustedSender: not in list");
 
-        emit TrustedSenderUpdated(
-            chainId,
-            trustedSender,
-            false /// removed from list
-        );
+        emit TrustedSenderUpdated(chainId, trustedSender, false);
+        /// removed from list
     }
 
     /// @dev Removes trusted senders from the list
     /// @param _trustedSenders The list of trusted senders to remove
-    function _removeTrustedSenders(
-        TrustedSender[] memory _trustedSenders
-    ) internal {
+    function _removeTrustedSenders(TrustedSender[] memory _trustedSenders) internal {
         unchecked {
             for (uint256 i = 0; i < _trustedSenders.length; i++) {
-                _removeTrustedSender(
-                    _trustedSenders[i].addr,
-                    _trustedSenders[i].chainId
-                );
+                _removeTrustedSender(_trustedSenders[i].addr, _trustedSenders[i].chainId);
             }
         }
     }
@@ -102,32 +77,22 @@ contract WormholeTrustedSender is IWormholeTrustedSender {
     /// @notice returns whether or not the address is in the trusted senders list for a given chain
     /// @param chainId The wormhole chain id to check
     /// @param addr The address to check
-    function isTrustedSender(
-        uint16 chainId,
-        bytes32 addr
-    ) public view override returns (bool) {
+    function isTrustedSender(uint16 chainId, bytes32 addr) public view override returns (bool) {
         return trustedSenders[chainId].contains(addr);
     }
 
     /// @notice returns whether or not the address is in the trusted senders list for a given chain
     /// @param chainId The wormhole chain id to check
     /// @param addr The address to check
-    function isTrustedSender(
-        uint16 chainId,
-        address addr
-    ) public view override returns (bool) {
+    function isTrustedSender(uint16 chainId, address addr) public view override returns (bool) {
         return isTrustedSender(chainId, addressToBytes(addr));
     }
 
     /// @notice returns the list of trusted senders for a given chain
     /// @param chainId The wormhole chain id to check
     /// @return The list of trusted senders
-    function allTrustedSenders(
-        uint16 chainId
-    ) external view override returns (bytes32[] memory) {
-        bytes32[] memory trustedSendersList = new bytes32[](
-            trustedSenders[chainId].length()
-        );
+    function allTrustedSenders(uint16 chainId) external view override returns (bytes32[] memory) {
+        bytes32[] memory trustedSendersList = new bytes32[](trustedSenders[chainId].length());
 
         unchecked {
             for (uint256 i = 0; i < trustedSendersList.length; i++) {
@@ -142,9 +107,7 @@ contract WormholeTrustedSender is IWormholeTrustedSender {
     /// then to a bytes32 *left* aligns it, so we right shift to get the proper data
     /// @param addr The address to convert
     /// @return The address as a bytes32
-    function addressToBytes(
-        address addr
-    ) public pure override returns (bytes32) {
+    function addressToBytes(address addr) public pure override returns (bytes32) {
         return bytes32(bytes20(addr)) >> 96;
     }
 }
