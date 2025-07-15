5d62d9fbe3d5650eb6d5d5c69156b4cebdab745d
Ana Bittencourt <anajuliabit@gmail.com>
2025-07-14 21:24:52 -0700

    feat: set addresses for InitiateStakingRewards proposal in BaseTest

--- test/BaseTest.t.sol
+++ test/BaseTest.t.sol
@@ -23,6 +23,9 @@ abstract contract BaseTest is Test {
         // Create and execute the multisig proposal
         proposal = new InitiateStakingRewards();
 
+        // Set the addresses for the proposal
+        proposal.setAddresses(addresses);
+
         // Deploy any necessary contracts
         proposal.deploy();
 
